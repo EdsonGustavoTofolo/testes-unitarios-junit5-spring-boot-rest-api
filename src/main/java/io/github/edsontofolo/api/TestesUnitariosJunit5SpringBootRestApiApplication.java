b10f818395a0ad8ae34dbe2725fd2e224c17fb95
Edson Tofolo <edsontofolo@gmail.com>
2022-06-28 15:33:06 -0300

    first commit

---
+++ src/main/java/io/github/edsontofolo/api/TestesUnitariosJunit5SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.edsontofolo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesUnitariosJunit5SpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestesUnitariosJunit5SpringBootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/edsontofolo/api/TestesUnitariosJunit5SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.edsontofolo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesUnitariosJunit5SpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
